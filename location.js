e410006ea54236b28a2eb34890fb51438bb57d87
soundestmammal <robert.checco@gmail.com>
2017-04-25 22:00:29 -0400

    Add locate position function. WU API now saved

---
+++ location.js
@@ -0,0 +1,21 @@
+var Geo = {};
+if (navigator.geolocation) {
+	navigator.geolocation.getCurrentPosition(success , error);
+}
+else {
+	alert("Geolocation is not supported");
+}
+function error() {
+	alert("That's weird! We couldn't find you!");
+}
+function success(position) {
+      Geo.lat = position.coords.latitude;
+      Geo.lng = position.coords.longitude;
+
+//API Key 9a62d2eb73fec908
+
+var key = 9a62d2eb73fec908
+var Weather = "http://api.wunderground.com/api/”+ key +”/forecast/geolookup/conditions/q/" + Geo.lat + "," + Geo.lng + ".json";
+
+
+
