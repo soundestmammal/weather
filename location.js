dffb2394fabafc4825d7e63897decca61b4e1ec9
Robert Checco <robert.checco@gmail.com>
2019-09-16 00:31:34 -0400

    Old Jquery code

--- location.js
+++ location.js
@@ -24,3 +24,17 @@ success : function(data) {
 // get all the information
 }
 });
+
+var location =data['location']['city'];
+var temp = data['current_observation']['temp_f'];
+var img = data['current_observation']['icon_url'];
+var desc = data['current_observation']['weather'];
+var wind = data['current_observation']['wind_string'];
+
+//setting the spans to the correct parameters
+$('#location').html(location);
+$('#temp').html(temp);
+$('#desc').html(desc);
+$('#wind').html(wind);
+//filling the image src attribute with the image url
+$('#img').attr('src', img);
